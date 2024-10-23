eca1aec3434a4029ada4dc70d07f28b375ab10e2
Sergei Shishkalov <sergeyshishkalov@gmail.com>
2024-10-23 08:11:16 +0300

    Add support for more images

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 const imageH = () => {
-  return {width: 1200, height: 800, color: '#FF4081'}
+  return {width: 1200, height: 900, color: '#FF4081'} // dark green, 4:3, vertical
 }
 const imageV = () => {
-  return {width: 800, height: 1200, color: '#56B8AE'}
+  return {width: 900, height: 1200, color: '#56B8AE'} // light-green, 3:4, horizontal
 }
 const imageVV = () => {
-  return {width: 1600, height: 900, color: '#30716A'}
+  return {width: 1600, height: 900, color: '#30716A'} // pink, 16:9, horizontal
 }
 const imageS = () => {
-  return {width: 800, height: 800, color: '#F2F2F2'}
+  return {width: 800, height: 800, color: '#F2F2F2'} // grey, square
 }
+const imageHH = () => {
+  return {width: 900, height: 1600, color: '#d388b9'} // magenta, 9:16, vertical
+}
+
+let SMALL_IMAGES_COUNT = 0
+let VERY_SMALL_IMAGES_COUNT = 0
+let ALL_IMAGES_COUNT = 0
+let OPTIMAL_DIFF_SUM = 0
+let OPTIMAL_NUMS = 0
 
 const renderPreviews = ($container, images) => {
   const $wrapper = $('<div class="wrapper" />')
   const $wrapperInner = $('<div class="wrapper-inner" />')
   $wrapper.append($wrapperInner)
 
-  const previews = Imagallery(images)
+  const { previews, aspectRatio, direction, diff } = Imagallery(images)
+
+  console.log(aspectRatio)
+  console.log(direction)
+
+  if (diff) {
+    OPTIMAL_NUMS++
+    OPTIMAL_DIFF_SUM += diff
+  }
+
+  ALL_IMAGES_COUNT += previews.length
+  SMALL_IMAGES_COUNT += previews.filter(p => p.width < 20).length
+  VERY_SMALL_IMAGES_COUNT += previews.filter(p => p.width < 15).length
 
   for (let preview of previews) {
     let $preview = $('<div class="preview" />').css({width: `${preview.width}%`, paddingBottom: `${preview.height}%`})
     let $previewInner = $('<div class="preview-inner" />')
-    let $previewImg = $('<div class="preview-img" />').css({'background-color': preview.color})
+    let $previewImg = $('<div class="preview-img" />').css({'background-color': preview.color || '#ccc'})
     $previewInner.append($previewImg)
     $preview.append($previewInner)
     $wrapperInner.append($preview)
@@ -36,7 +57,9 @@ const renderInCol = (fn) => {
   fn($col)
 }
 
-const items = [imageV, imageH, imageS, imageVV, imageH]
+console.time('previewing')
+
+const items = [imageV, imageH, imageS, imageVV, imageH, imageHH]
 
 renderInCol(($col) => {
   for (let item of items) {
@@ -79,9 +102,38 @@ renderInCol(($col) => {
     for (let secondItem of items) {
       for (let thirdItem of items) {
         for (let fItem of items) {
-          renderPreviews($col, [item(), thirdItem(), secondItem(), item(), fItem(), secondItem(), secondItem(), thirdItem(), item()])
+          renderPreviews($col, [secondItem(), item(), secondItem(), thirdItem(), fItem()])
         }
       }
     }
   }
 })
+
+renderInCol(($col) => {
+  for (let item of items) {
+    for (let secondItem of items) {
+      for (let thirdItem of items) {
+        for (let fItem of items) {
+          renderPreviews($col, [item(), thirdItem(), secondItem(), item(), fItem(), secondItem(), secondItem(), thirdItem(), thirdItem(), item()])
+        }
+      }
+    }
+  }
+})
+renderInCol(($col) => {
+  for (let item of items) {
+    for (let secondItem of items) {
+      for (let thirdItem of items) {
+        for (let fItem of items) {
+          renderPreviews($col, [item(), thirdItem(), secondItem(), item(), fItem(), secondItem(), secondItem(), thirdItem(), item(), item(), thirdItem(), secondItem(), item(), fItem(), secondItem(), secondItem(), thirdItem(), item(), fItem(), secondItem()])
+        }
+      }
+    }
+  }
+})
+console.timeEnd('previewing')
+
+console.log('all images', ALL_IMAGES_COUNT)
+console.log('images with width < 20%', SMALL_IMAGES_COUNT)
+console.log('images with width < 15%', VERY_SMALL_IMAGES_COUNT)
+console.log('averageDiff', OPTIMAL_DIFF_SUM / OPTIMAL_NUMS)
